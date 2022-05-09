1414ff24d91e7dad71957620ff7fe7f9c191befd
Samuelson Brito <samuelsonma@gmail.com>
2022-05-08 23:55:23 -0400

    Testar outros usuários

--- scripts/run.js
+++ scripts/run.js
@@ -6,14 +6,14 @@ const main = async () => {
 
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
-    
+
     console.log("Contract deployed to:", waveContract.address);
     console.log("Contract deployed by:", owner.address);
 
     let waveCount;
     waveCount = await waveContract.getTotalWaves();
 
-    let waveTxn = await waveContract.wave();
+    let waveTxn = await waveContract.connect(randomPerson).wave();
     await waveTxn.wait();
 
     waveCount = await waveContract.getTotalWaves();
